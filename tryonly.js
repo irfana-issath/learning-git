96415bbf2e7ce6205a37c521784f38fe58728d2c
irfanaissath <irfanaissath755@gmail.com>
2025-07-23 16:38:12 +0530

    javascript.html edited

--- tryonly.js
+++ tryonly.js
@@ -1,6 +1,19 @@
 //  document.writeln("heeey")
 // console.log("shinguuuuuu")
 // console.log(30+20)
- 
+//  const forChange=()=>{
+//     document.getElementById("change").innerHTML="Welcome to wezlonHub"
+//  }
 
+// const arr=[10,11,20,22,30,33,40,44,50]
+
+// const newArr=arr.map((value)=>{
+// return value+5
+// })
+
+// const newArr=arr.filter((value)=>{
+//     const result= value%2==0
+//     return result
+// })
+// console.log(newArr)
 
